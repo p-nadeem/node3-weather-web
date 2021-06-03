42e65da003063727534dce312ec0dc8b4e29f629
Nadeem <pnadheem@gmail.com>
2021-06-03 12:43:28 +0530

    new

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     console.log(location)
     if (location) {
-        fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        fetch('/weather?address=' + location).then((response) => {
             response.json().then((data) => {
                 message.textContent = data.forcast
             })
